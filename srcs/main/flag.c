f0f58fcc5a40dcfaafa4891ef07c56d897a8af38
isaenesuslu <isaenesuslu@hotmail.com>
2026-03-16 23:53:42 +0300

    refactor: optimize and update code in srcs

---
+++ srcs/main/flag.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bench.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybalkan <ybalkan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 21:00:23 by ybalkan           #+#    #+#             */
+/*   Updated: 2026/03/16 23:47:37 by ybalkan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ps.h"
+#include "algo.h"
+#include "ops.h"
+
+void	ps_dispatch_sort(t_node **a, t_node **b, int size, int *move_count)
+{
+	if (g_mode == MODE_SIMPLE || (g_mode == MODE_ADAPTIVE && size <= 5))
+	{
+		if (size == 2)
+			sa(a, true, move_count);
+		else if (size == 3)
+			sort_three(a, move_count);
+		else if (size > 1)
+			ps_sort_small(a, b, move_count);
+	}
+	else
+		ps_sort_engine(a, b, move_count);
+}
+
+void	ps_print_bench(int n)
+{
+	char	buf[12];
+	int		i;
+
+	write(1, "Total moves: ", 13);
+	i = 0;
+	if (n == 0)
+		buf[i++] = '0';
+	while (n > 0)
+	{
+		buf[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+	while (i-- > 0)
+		write(1, &buf[i], 1);
+	write(1, "\n", 1);
+}
