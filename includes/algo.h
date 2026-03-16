1ee4a264e6b0ea22846039f58a614e3f43d1f095
isaenesuslu <isaenesuslu@hotmail.com>
2026-03-16 14:10:34 +0300

    refactor: optimize and update code in includes

--- includes/algo.h
+++ includes/algo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   algo.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybalkan <ybalkan@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*   By: ybalkan <ybalkan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 10:48:31 by ybalkan           #+#    #+#             */
-/*   Updated: 2026/03/16 12:02:17 by ybalkan          ###   ########.fr       */
+/*   Updated: 2026/03/16 14:09:01 by ybalkan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 
 # include "ps.h"
 
-/* Sorting Algorithms */
-void	ps_sort_small(t_node **stack_a, t_node **stack_b);
-void	sort_three(t_node **stack_a);
-void	ps_sort_engine(t_node **stack_a, t_node **stack_b);
+void	ps_sort_small(t_node **stack_a, t_node **stack_b, int *move_count);
+void	sort_three(t_node **stack_a, int *move_count);
+void	ps_sort_engine(t_node **stack_a, t_node **stack_b, int *move_count);
 
-/* Algorithm Utilities */
 void	ps_set_rank_indices(t_node *stack);
-void	ps_push_cheapest_to_b(t_node **a, t_node **b);
-void	ps_finalize_rotation(t_node **a);
+void	ps_push_cheapest_to_b(t_node **a, t_node **b, int *move_count);
+void	ps_finalize_rotation(t_node **a, int *move_count);
 int		find_max_val(t_node *stack);
 int		find_min_pos(t_node *stack);
 int		find_max_pos(t_node **stack);
