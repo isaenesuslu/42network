3f2eb744a8768d45611bc6ab3f786fdc97382fab
isaenesuslu <isaenesuslu@hotmail.com>
2026-03-16 14:11:29 +0300

    refactor: optimize and update code in srcs

--- srcs/algo/short.c
+++ srcs/algo/short.c
@@ -13,7 +13,7 @@
 #include "algo.h"
 #include "ops.h"
 
-void	sort_three(t_node **a)
+void	sort_three(t_node **a, int *move_count)
 {
 	int	max;
 
@@ -21,23 +21,23 @@ void	sort_three(t_node **a)
 		return ;
 	max = find_max_val(*a);
 	if ((*a)->value == max)
-		ra(a, true);
+		ra(a, true, move_count);
 	else if ((*a)->next->value == max)
-		rra(a, true);
+		rra(a, true, move_count);
 	if ((*a)->value > (*a)->next->value)
-		sa(a, true);
+		sa(a, true, move_count);
 }
 
-void	ps_sort_small(t_node **a, t_node **b)
+void	ps_sort_small(t_node **a, t_node **b, int *move_count)
 {
 	while (ps_get_size(*a) > 3 && !ps_is_sorted(*a))
-		ps_push_cheapest_to_b(a, b);
-	sort_three(a);
+		ps_push_cheapest_to_b(a, b, move_count);
+	sort_three(a, move_count);
 	while (*b)
-		pa(a, b, true);
+		pa(a, b, true, move_count);
 }
 
-void	ps_push_cheapest_to_b(t_node **a, t_node **b)
+void	ps_push_cheapest_to_b(t_node **a, t_node **b, int *move_count)
 {
 	int	min_pos;
 	int	size;
@@ -47,12 +47,12 @@ void	ps_push_cheapest_to_b(t_node **a, t_node **b)
 	if (min_pos > size / 2)
 	{
 		while (min_pos++ < size)
-			rra(a, true);
+			rra(a, true, move_count);
 	}
 	else
 	{
 		while (min_pos-- > 0)
-			ra(a, true);
+			ra(a, true, move_count);
 	}
-	pb(a, b, true);
+	pb(a, b, true, move_count);
 }
