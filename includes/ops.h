72fbdefe87ca41b1fabc779480ef08fbd7f0f020
isaenesuslu <isaenesuslu@hotmail.com>
2026-03-22 16:25:49 +0300

    refactor: optimize and update code in includes

--- includes/ops.h
+++ includes/ops.h
@@ -16,18 +16,14 @@
 # include "ps.h"
 
 void	sa(t_node **stack_a, bool print, int *move_count);
-void	sb(t_node **stack_b, bool print, int *move_count);
-void	ss(t_node **stack_a, t_node **stack_b, bool print, int *move_count);
 
 void	pa(t_node **stack_a, t_node **stack_b, bool print, int *move_count);
 void	pb(t_node **stack_a, t_node **stack_b, bool print, int *move_count);
 
 void	ra(t_node **stack_a, bool print, int *move_count);
 void	rb(t_node **stack_b, bool print, int *move_count);
-void	rr(t_node **stack_a, t_node **stack_b, bool print, int *move_count);
 
 void	rra(t_node **stack_a, bool print, int *move_count);
 void	rrb(t_node **stack_b, bool print, int *move_count);
-void	rrr(t_node **stack_a, t_node **stack_b, bool print, int *move_count);
 
 #endif
